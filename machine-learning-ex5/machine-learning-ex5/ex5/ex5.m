86f71fa1189af7394265df8c66169532cf3fefd6
jonchan <jchchan91@gmail.com>
2016-09-18 17:31:57 -0400

    Ex5, 1.3, Regulairzed Linear Regression Gradient

--- machine-learning-ex5/machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/machine-learning-ex5/ex5/ex5.m
@@ -54,7 +54,7 @@ fprintf(['Cost at theta = [1 ; 1]: %f '...
          '\n(this value should be about 303.993192)\n'], J);
 % 
 % fprintf('Program paused. Press enter to continue.\n');
-return;
+% pause;
 
 %% =========== Part 3: Regularized Linear Regression Gradient =============
 %  You should now implement the gradient for regularized linear 
@@ -68,9 +68,9 @@ fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
          '\n(this value should be about [-15.303016; 598.250744])\n'], ...
          grad(1), grad(2));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
+return;
 
 %% =========== Part 4: Train Linear Regression =============
 %  Once you have implemented the cost and gradient correctly, the
--- machine-learning-ex5/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -20,15 +20,29 @@ grad = zeros(size(theta));
 %
 
 % Compute cost
-e = hypothesis(X, theta) - y;
+e = hypothesis(X, theta) - y; % m x 1
 J = (e' * e) / (2 * m);
 
 % Add the regularization
 reg = (theta(2:end)' * theta(2:end)) * lambda / (2 * m);
 J = J + reg;
 
-
-
+% Compute the gradient
+% nx1 = nxm * mx1
+grad = X' * e / m; % Compute the unregularized gradient
+grad_reg = lambda * theta(2:end) / m;
+grad(2:end) = grad(2:end) + grad_reg;
+% for j = 1:m
+% for j = 1:length(theta)
+%     % 1x1 = 1xm * mx1
+%     grad(j) = X(:,j)' * e / m;
+%     % nx1 = 1xn * mx1
+%     grad = grad + (X(j,:) * e / m);
+%     grad(1) = grad(1) + (X(j,1) * e / m); 
+%     grad(2:end) = grad(2:end) + (X(j,2:end) * e / m); % Compute the unregularized gradient
+%     grad_reg = lambda * theta(j) / m; % Compute regularization for gradient
+%     grad(j) = grad(j) + grad_reg;
+% end
 
 % =========================================================================
 
